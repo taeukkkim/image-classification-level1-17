709848c07e78dd29579870ae8ac8bae412c29159
seokmin <soos3121@gmail.com>
2021-08-30 15:30:32 +0900

    feat: dataset 초기화, mymodel 추가

--- T2001/dataset.py
+++ T2001/dataset.py
@@ -105,41 +105,59 @@ class AgeLabels(int, Enum):
         else:
             return cls.OLD
 
-class TrainDataset(Dataset):
-    mask_list = ['Wear', 'Incorrect', 'Not Wear']
-    gender_list = ['Male', 'Female']
-    age_list = ['age < 30', '30 <= age < 60', '60 <= age']
 
-    def __init__(self, train_pd, transform = transforms.ToTensor(), mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
-        self.X = train_pd['img_path']
-        self.y = train_pd['y']
-        self.transform = transform
+class MaskBaseDataset(Dataset):
+    num_classes = 3 * 2 * 3
 
-    def __getitem__(self, index):
-        image = Image.open(self.X[index])
+    _file_names = {
+        "mask1": MaskLabels.MASK,
+        "mask2": MaskLabels.MASK,
+        "mask3": MaskLabels.MASK,
+        "mask4": MaskLabels.MASK,
+        "mask5": MaskLabels.MASK,
+        "incorrect_mask": MaskLabels.INCORRECT,
+        "normal": MaskLabels.NORMAL
+    }
 
-        if self.transform:
-            image = self.transform(image)
-        return image, self.y[index]
+    image_paths = []
+    mask_labels = []
+    gender_labels = []
+    age_labels = []
 
-    def __len__(self):
-        return len(self.X)
+    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
+        self.data_dir = data_dir
+        self.mean = mean
+        self.std = std
+        self.val_ratio = val_ratio
 
-    def get_mask_class(self, index):
-        return self.mask_list[self.y[index] // 6]
-    
-    def get_gender_class(self, index):
-        return self.gender_list[self.y[index] % 6 // 3]
-    
-    def get_age_class(self, index):
-        return self.gender_list[self.y[index] % 6 % 3]
+        self.transform = None
+        self.setup()
+        self.calc_statistics()
 
-    def get_entire_class(self, index):
-        mask_class = self.get_mask_class(index)
-        gender_class = self.get_gender_class(index)
-        age_class = self.get_age_class(index)
-        return { mask_class, gender_class, age_class }
-    
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        for profile in profiles:
+            if profile.startswith("."):  # "." 로 시작하는 파일은 무시합니다
+                continue
+
+            img_folder = os.path.join(self.data_dir, profile)
+            for file_name in os.listdir(img_folder):
+                _file_name, ext = os.path.splitext(file_name)
+                if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                    continue
+
+                img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                mask_label = self._file_names[_file_name]
+
+                id, gender, race, age = profile.split("_")
+                gender_label = GenderLabels.from_str(gender)
+                age_label = AgeLabels.from_number(age)
+
+                self.image_paths.append(img_path)
+                self.mask_labels.append(mask_label)
+                self.gender_labels.append(gender_label)
+                self.age_labels.append(age_label)
+
     def calc_statistics(self):
         has_statistics = self.mean is not None and self.std is not None
         if not has_statistics:
@@ -154,17 +172,127 @@ class TrainDataset(Dataset):
             self.mean = np.mean(sums, axis=0) / 255
             self.std = (np.mean(squared, axis=0) - self.mean ** 2) ** 0.5 / 255
 
-    def split_dataset(self, val_ratio) -> Tuple[Subset, Subset]:
+    def set_transform(self, transform):
+        self.transform = transform
+
+    def __getitem__(self, index):
+        assert self.transform is not None, ".set_tranform 메소드를 이용하여 transform 을 주입해주세요"
+
+        image = self.read_image(index)
+        mask_label = self.get_mask_label(index)
+        gender_label = self.get_gender_label(index)
+        age_label = self.get_age_label(index)
+        multi_class_label = self.encode_multi_class(mask_label, gender_label, age_label)
+
+        image_transform = self.transform(image)
+        return image_transform, multi_class_label
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def get_mask_label(self, index) -> MaskLabels:
+        return self.mask_labels[index]
+
+    def get_gender_label(self, index) -> GenderLabels:
+        return self.gender_labels[index]
+
+    def get_age_label(self, index) -> AgeLabels:
+        return self.age_labels[index]
+
+    def read_image(self, index):
+        image_path = self.image_paths[index]
+        return Image.open(image_path)
+
+    @staticmethod
+    def encode_multi_class(mask_label, gender_label, age_label) -> int:
+        return mask_label * 6 + gender_label * 3 + age_label
+
+    @staticmethod
+    def decode_multi_class(multi_class_label) -> Tuple[MaskLabels, GenderLabels, AgeLabels]:
+        mask_label = (multi_class_label // 6) % 3
+        gender_label = (multi_class_label // 3) % 2
+        age_label = multi_class_label % 3
+        return mask_label, gender_label, age_label
+
+    @staticmethod
+    def denormalize_image(image, mean, std):
+        img_cp = image.copy()
+        img_cp *= std
+        img_cp += mean
+        img_cp *= 255.0
+        img_cp = np.clip(img_cp, 0, 255).astype(np.uint8)
+        return img_cp
+
+    def split_dataset(self) -> Tuple[Subset, Subset]:
         """
         데이터셋을 train 과 val 로 나눕니다,
         pytorch 내부의 torch.utils.data.random_split 함수를 사용하여
         torch.utils.data.Subset 클래스 둘로 나눕니다.
         구현이 어렵지 않으니 구글링 혹은 IDE (e.g. pycharm) 의 navigation 기능을 통해 코드를 한 번 읽어보는 것을 추천드립니다^^
         """
-        n_val = int(len(self) * val_ratio)
+        n_val = int(len(self) * self.val_ratio)
         n_train = len(self) - n_val
         train_set, val_set = random_split(self, [n_train, n_val])
         return train_set, val_set
+
+
+class MaskSplitByProfileDataset(MaskBaseDataset):
+    """
+        train / val 나누는 기준을 이미지에 대해서 random 이 아닌
+        사람(profile)을 기준으로 나눕니다.
+        구현은 val_ratio 에 맞게 train / val 나누는 것을 이미지 전체가 아닌 사람(profile)에 대해서 진행하여 indexing 을 합니다
+        이후 `split_dataset` 에서 index 에 맞게 Subset 으로 dataset 을 분기합니다.
+    """
+
+    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
+        self.indices = defaultdict(list)
+        super().__init__(data_dir, mean, std, val_ratio)
+
+    @staticmethod
+    def _split_profile(profiles, val_ratio):
+        length = len(profiles)
+        n_val = int(length * val_ratio)
+
+        val_indices = set(random.choices(range(length), k=n_val))
+        train_indices = set(range(length)) - val_indices
+        return {
+            "train": train_indices,
+            "val": val_indices
+        }
+
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        profiles = [profile for profile in profiles if not profile.startswith(".")]
+        split_profiles = self._split_profile(profiles, self.val_ratio)
+
+        cnt = 0
+        for phase, indices in split_profiles.items():
+            for _idx in indices:
+                profile = profiles[_idx]
+                img_folder = os.path.join(self.data_dir, profile)
+                for file_name in os.listdir(img_folder):
+                    _file_name, ext = os.path.splitext(file_name)
+                    if _file_name not in self._file_names:  # "." 로 시작하는 파일 및 invalid 한 파일들은 무시합니다
+                        continue
+
+                    img_path = os.path.join(self.data_dir, profile, file_name)  # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                    mask_label = self._file_names[_file_name]
+
+                    id, gender, race, age = profile.split("_")
+                    gender_label = GenderLabels.from_str(gender)
+                    age_label = AgeLabels.from_number(age)
+
+                    self.image_paths.append(img_path)
+                    self.mask_labels.append(mask_label)
+                    self.gender_labels.append(gender_label)
+                    self.age_labels.append(age_label)
+
+                    self.indices[phase].append(cnt)
+                    cnt += 1
+
+    def split_dataset(self) -> List[Subset]:
+        return [Subset(self, indices) for phase, indices in self.indices.items()]
+
 
 class TestDataset(Dataset):
     def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
--- T2001/model.py
+++ T2001/model.py
@@ -2,7 +2,47 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class MyModel(nn.Module):
+    def __init__(self, num_classes):
 
+        super().__init__()
+        def residual_layer(input_channel, count):
+            res_layer = nn.Sequential(
+                nn.Conv2d(input_channel, input_channel/2, kernel_size=1),
+                nn.ReLU(),
+                nn.Dropout(p=0.15),
+                nn.Conv2d(input_channel/2, input_channel, kernel_size=3, padding=1),
+            )
+            def residual_func(input_value):
+                for _ in range(count):
+                    input_value = F.relu(input_value + res_layer(input_value))
+                return input_value
+            return residual_func
+
+        def net_layer(output_channel):
+            return nn.Sequential(
+                nn.Conv2d(output_channel/2, output_channel, kernel_size=3, stride=2, padding=1),
+                nn.ReLU()
+            )
+        
+        count_num = [1,2,8,8,4]
+        channel = 32
+        layers = []
+        for count in count_num:
+            channel = channel * 2
+            layers.append(net_layer(channel))
+            layers.append(residual_layer(channel, count))
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            *layers,
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.features(x)
 class BaseModel(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
@@ -33,21 +73,3 @@ class BaseModel(nn.Module):
         x = x.view(-1, 128)
         return self.fc(x)
 
-
-# Custom Model Template
-class MyModel(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
-
-    def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-        return x
