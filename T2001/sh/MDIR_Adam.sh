98073464bc2ebe64ba853e1b55a0f54db9c50c80
seokmin <soos0228@naver.com>
2021-08-31 16:37:50 +0900

    style: Edit sh filename

---
+++ T2001/sh/MDIR_Adam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python /opt/ml/image-classification-level1-17/T2001/train.py \
+--load_params True \
+--exist_ok False \
+--epochs 3 \
+--lr 1e-4 \
+--batch_size 64 \
+--valid_batch_size 64 \
+--optimizer Adam \
+--log_interval 20 \
+--model multilabel_dropout_IR \
+--name MDIR_Adam_Cosine 
--- T2001/sh/MDIR_Adam_Custom.sh
+++ T2001/sh/MDIR_Adam_Custom.sh
@@ -3,10 +3,12 @@
 python /opt/ml/image-classification-level1-17/T2001/train.py \
 --load_params True \
 --exist_ok False \
---epochs 3 \
---batch_size 32 \
---valid_batch_size 32 \
+--epochs 5000 \
+--lr 1e-2 \
+--batch_size 64 \
+--valid_batch_size 64 \
 --optimizer Adam \
 --log_interval 20 \
 --model multilabel_dropout_IR \
---name MDIR_Adam_Cosine
+--name MDIR_Adam_StepLR_Custom \
+--augmentation CustomAugmentation
