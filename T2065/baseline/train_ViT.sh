b835d1f84e5f42edc3aa23c5f7c313bbdafa1d6a
taeukkkim <ktu9682@gmail.com>
2021-08-31 08:04:49 +0000

    train_ViT ver2

--- T2065/baseline/train_ViT.sh
+++ T2065/baseline/train_ViT.sh
@@ -3,15 +3,15 @@ python train.py \
          --dataset MaskSplitByProfileDataset \
          --augmentation get_transforms \
          --resize 224 224 \
-         --lr 0.00003 \
-         --lr_decay_step 50 \
+         --lr 0.00005 \
+         --lr_decay_step 2 \
          --lr_gamma 0.7 \
          --batch_size 32 \
          --valid_batch_size 32 \
          --model ViT \
-         --model_version vit_small_patch16_224\
+         --model_version vit_base_patch16_224_in21k\
          --optimizer Adam \
          --criterion focal\
          --log_interval 100\
-         --name ViT_vit_small_patch16_224_0831
+         --name ViT_vit_base_patch16_224_in21k_0831
          
