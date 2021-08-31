f74ee9bf784cc79e1afae1b5d62f9900982dbf9a
taeukkkim <ktu9682@gmail.com>
2021-08-31 07:27:02 +0000

    0.7023_2

--- T2065/baseline/train_effnet.sh
+++ T2065/baseline/train_effnet.sh
@@ -4,16 +4,16 @@ python train.py \
          --augmentation get_transforms \
          --resize 512 384 \
          --lr 0.00003 \
-         --lr_decay_step 2 \
+         --lr_decay_step 50 \
          --lr_gamma 0.7 \
          --batch_size 32 \
          --valid_batch_size 32 \
          --model EfficientNet \
          --model_version b3\
          --optimizer Adam \
-         --criterion cross_entropy\
+         --criterion focal\
          --log_interval 100\
-         --name EfficientNet_b3_0831
+         --name EfficientNet_b3_0831_2
          
 
 # python inference.py \
