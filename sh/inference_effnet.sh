01b2c2e474d3106439077edd9ce1137dbc63e8b8
seokmin-kang <soos0228@naver.com>
2021-09-02 15:53:41 +0900

    fix: master 충돌해결

--- sh/inference_effnet.sh
+++ sh/inference_effnet.sh
@@ -2,27 +2,22 @@ python ../inference.py \
          --batch_size 32 \
          --resize 512 384 \
          --model EfficientNet \
-<<<<<<< HEAD:baseline/inference_effnet.sh
          --model_version tf_efficientnet_b7 \
          --name T2065/EfficientNet_b7_0902_mask \
          --output_name EfficientNet_b7_0902_mask
 
-python inference.py \
+python ../inference.py \
          --batch_size 32 \
          --resize 512 384 \
          --model EfficientNet \
          --model_version tf_efficientnet_b7 \
-         --name T2065/EfficientNet_b7_0902_gender \
+         --name EfficientNet_b7_0902_gender \
          --output_name EfficientNet_b7_0902_gender
 
-python inference.py \
+python ../inference.py \
          --batch_size 32 \
          --resize 512 384 \
          --model EfficientNet \
          --model_version tf_efficientnet_b7 \
-         --name T2065/EfficientNet_b7_0902_age \
+         --name EfficientNet_b7_0902_age \
          --output_name EfficientNet_b7_0902_age
-=======
-         --model_version b3_pruned \
-         --name EfficientNet_b3_pruned_0831 
->>>>>>> refactor:sh/inference_effnet.sh
--- sh/train_resnet.sh
+++ sh/train_resnet.sh
@@ -1,5 +1,4 @@
-<<<<<<< HEAD:baseline/train_resnet.sh
-python train.py \
+python ../train.py \
          --epochs 15 \
          --dataset MaskSplitStratifyDataset \
          --label mask\
@@ -14,17 +13,12 @@ python train.py \
          --optimizer Adam \
          --criterion focal\
          --log_interval 100\
-         --name T2065/Resnet18_0901_Stratify_mask
+         --name Resnet18_0901_Stratify_mask
          
 
-python train.py \
+python ../train.py \
          --epochs 15 \
          --dataset MaskSplitStratifyDataset \
-=======
-python ../train.py \
-         --epochs 10 \
-         --dataset MaskSplitByProfileDataset \
->>>>>>> refactor:sh/train_resnet.sh
          --label gender\
          --augmentation get_transforms \
          --resize 512 384 \
@@ -37,20 +31,12 @@ python ../train.py \
          --optimizer Adam \
          --criterion focal\
          --log_interval 100\
-<<<<<<< HEAD:baseline/train_resnet.sh
-         --name T2065/Resnet18_0901_Stratify_gender
+         --name Resnet18_0901_Stratify_gender
 
-
-python train.py \
-         --epochs 15 \
-         --dataset MaskSplitStratifyDataset \
-=======
-         --name Resnet18_0901_gender
 
 python ../train.py \
-         --epochs 10 \
-         --dataset MaskSplitByProfileDataset \
->>>>>>> refactor:sh/train_resnet.sh
+         --epochs 15 \
+         --dataset MaskSplitStratifyDataset \
          --label age\
          --augmentation get_transforms \
          --resize 512 384 \
@@ -63,26 +49,4 @@ python ../train.py \
          --optimizer Adam \
          --criterion focal\
          --log_interval 100\
-<<<<<<< HEAD:baseline/train_resnet.sh
-         --name T2065/Resnet18_0901_Stratify_age
-=======
-         --name Resnet18_0901_age
-
-# python train.py \
-#          --epochs 20 \
-#          --dataset MaskSplitByProfileDataset \
-#          --label mask\
-#          --augmentation get_transforms \
-#          --resize 512 384 \
-#          --lr 0.00003 \
-#          --lr_decay_step 50 \
-#          --lr_gamma 0.7 \
-#          --batch_size 32 \
-#          --valid_batch_size 32 \
-#          --model Resnet18 \
-#          --optimizer Adam \
-#          --criterion focal\
-#          --log_interval 100\
-#          --name T2065/Resnet18_0901_mask
-         
->>>>>>> refactor:sh/train_resnet.sh
+         --name Resnet18_0901_Stratify_age
